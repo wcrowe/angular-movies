2374a1f41a8f4c834ffcfc40a361c7db9fa1d9b6
wcrowe <crowe.will@gmail.com>
2021-02-17 22:34:03 -0500

    Fixed markers

--- movies-ui/src/app/utilities/map/map.component.ts
+++ movies-ui/src/app/utilities/map/map.component.ts
@@ -1,6 +1,25 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import * as L from 'leaflet';
 import { latLng, LeafletMouseEvent, marker, Marker, tileLayer } from 'leaflet';
 import { coordinatesMap } from './coordinate';
+
+
+// Marker fix for leaflet
+// https://www.digitalocean.com/community/tutorials/angular-angular-and-leaflet-marker-service
+const iconRetinaUrl = 'assets/marker-icon-2x.png';
+const iconUrl = 'assets/marker-icon.png';
+const shadowUrl = 'assets/marker-shadow.png';
+const iconDefault = L.icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+});
+L.Marker.prototype.options.icon = iconDefault;
 
 @Component({
   selector: 'app-map',
@@ -23,12 +42,12 @@ export class MapComponent implements OnInit {
 
   options = {
     layers: [
-      tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 18,
-      attribution: 'Angular Movies' })
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors'
+      })
     ],
-    zoom: 14,
-    center: latLng(18.473564631048617,  -69.93999481201173)
+    zoom: 7,
+    center: latLng([ 46.879966, -121.726909 ])
   };
 
   layers: Marker<any>[] = [];
@@ -37,9 +56,9 @@ export class MapComponent implements OnInit {
     const latitude = event.latlng.lat;
     const longitude = event.latlng.lng;
     console.log({latitude, longitude});
-    //this.layers = [];
-    //this.layers.push(marker([latitude, longitude]));
-    //this.onSelectedLocation.emit({latitude, longitude});
+    this.layers = [];
+    this.layers.push(marker([latitude, longitude]));
+    this.onSelectedLocation.emit({latitude, longitude});
   }
 
 }
